dce069a3ec37f74665fc0f5ed1dd27d93ca14dae
Oussama Amine Bentama <oa.bentama@gmail.com>
2020-11-27 21:06:19 +0100

    util commit

--- lib/util/database.dart
+++ lib/util/database.dart
@@ -3,7 +3,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 import 'dart:io';
-import 'model/model.dart';
+import '../model/model.dart';
 
 class DbHelper {
   // Tables
