39e168c337d9db73b4c80d26e60e021514eaa926
Oussama Amine Bentama <oa.bentama@gmail.com>
2020-11-27 21:03:47 +0100

    Model & Database

--- lib/database.dart
+++ lib/database.dart
@@ -6,20 +6,18 @@ import 'dart:io';
 import 'model/model.dart';
 
 class DbHelper {
-  
   // Tables
-  static String tblProduits = "produits";
-  static String tblCommandes = "commandes";
+  static String tblProduit = "produit";
+  static String tblCommande = "commande";
+  static String tblProduitCommande = "produitcommande";
 
-
-  // Fields of the 'produits' table :
+  // Fields of the 'produit' table :
   String idProduit = "idProduit";
   String titreProduit = "titreProduit";
   String prixProduit = "prixProduit";
   String descriptionProduit = "descriptionProduit";
 
-
-  // Fields of the 'commandes' table :
+  // Fields of the 'commande' table :
   String idCommande = "idCommande";
   String numTel = "numTel";
   String nomClient = "nomClient";
@@ -27,7 +25,12 @@ class DbHelper {
   String livraison = "livraison";
   String descriptionCommande = "descriptionCommande";
 
-
+  // Fields of the 'produitcommande' table :
+  String idProduitCommande = "idProduitCommande";
+  String titreProduitCommande = "titreProduitCommande";
+  String quantite = "quantite";
+  String idCommandep = "idCommandep";
+  String idProduitp = "idProduitp";
 
   static final DbHelper _dbHelper = DbHelper._internal();
 
@@ -56,80 +59,217 @@ class DbHelper {
   // Create database table
   void _createDb(Database db, int version) async {
     await db.execute(
-        "CREATE TABLE $tblProduits($idProduit INTEGER PRIMARY KEY, $titreProduit TEXT, " +
+        "CREATE TABLE $tblProduit($idProduit INTEGER PRIMARY KEY, $titreProduit TEXT, " +
             "$prixProduit INTEGER, " +
             "$descriptionProduit TEXT, )");
+    await db.execute(
+        "CREATE TABLE $tblCommande($idCommande INTEGER PRIMARY KEY, $numTel INTEGER, " +
+            "$nomClient TEXT, " +
+            "$dateTime TEXT, " +
+            "$livraison INTEGER, " +
+            "$descriptionCommande TEXT, )");
+    await db.execute(
+        "CREATE TABLE $tblProduitCommande($idProduitCommande INTEGER PRIMARY KEY, $titreProduitCommande TEXT, " +
+            "$quantite INTEGER, " +
+            "$idCommandep INTEGER, " +
+            "$idProduitp INTEGER, )");
   }
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////// Region Produit //////////////////////
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 
   Future<int> insertProduct(Produit produit) async {
     var r;
     Database db = await this.db;
     try {
-      r = await db.insert(tblProduits, produit.toMap());
+      r = await db.insert(tblProduit, produit.toMap());
     } catch (e) {
-      debugPrint("insertDoc: " + e.toString());
+      debugPrint("insertProduct: " + e.toString());
     }
     return r;
   }
 
   Future<List> getProducts() async {
     Database db = await this.db;
-    var r =
-        await db.rawQuery("SELECT * FROM $tblProduits ORDER BY $titreProduit ASC");
+    var r = await db
+        .rawQuery("SELECT * FROM $tblProduit ORDER BY $titreProduit ASC");
     return r;
   }
 
   Future<List> getProduct(int id) async {
     Database db = await this.db;
     var r = await db.rawQuery(
-        "SELECT * FROM $tblProduits WHERE $idProduit = " + id.toString() + "");
+        "SELECT * FROM $tblProduit WHERE $idProduit = " + id.toString() + "");
     return r;
   }
 
-  // Future<List> getDocFromStr(String payload) async {
-  //   List<String> p = payload.split("|");
-  //   if (p.length == 2) {
-  //     Database db = await this.db;
-  //     var r = await db.rawQuery("SELECT * FROM $tblDocs WHERE $docId = " +
-  //         p[0] +
-  //         " AND $docExpiration = '" +
-  //         p[1] +
-  //         "'");
-  //     return r;
-  //   } else
-  //     return null;
-  // }
+  Future<int> getProductsCount() async {
+    Database db = await this.db;
+    var r = Sqflite.firstIntValue(
+        await db.rawQuery("SELECT COUNT(*) FROM $tblProduit"));
+    return r;
+  }
 
-  // Future<int> getDocsCount() async {
-  //   Database db = await this.db;
-  //   var r = Sqflite.firstIntValue(
-  //       await db.rawQuery("SELECT COUNT(*) FROM $tblDocs"));
-  //   return r;
-  // }
+  Future<int> getMaxIdProduit() async {
+    Database db = await this.db;
+    var r = Sqflite.firstIntValue(
+        await db.rawQuery("SELECT MAX(idProduit) FROM $tblProduit"));
+    return r;
+  }
 
-  // Future<int> getMaxId() async {
-  //   Database db = await this.db;
-  //   var r = Sqflite.firstIntValue(
-  //       await db.rawQuery("SELECT MAX(id) FROM $tblDocs"));
-  //   return r;
-  // }
-
-  // Future<int> updateDoc(Doc doc) async {
-  //   var db = await this.db;
-  //   var r = await db
-  //       .update(tblDocs, doc.toMap(), where: "$docId = ?", whereArgs: [doc.id]);
-  //   return r;
-  // }
+  Future<int> updateProduct(Produit produit) async {
+    var db = await this.db;
+    var r = await db.update(tblProduit, produit.toMap(),
+        where: "$idProduit = ?", whereArgs: [produit.idProduit]);
+    return r;
+  }
 
   Future<int> deleteProduct(int id) async {
     var db = await this.db;
-    int r = await db.rawDelete("DELETE FROM $tblProduits WHERE $idProduit = $id");
+    int r =
+        await db.rawDelete("DELETE FROM $tblProduit WHERE $idProduit = $id");
     return r;
   }
 
-  // Future<int> deleteRows(String tbl) async {
-  //   var db = await this.db;
-  //   int r = await db.rawDelete("DELETE FROM $tbl");
-  //   return r;
-  // }
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////// Region Commande //////////////////////
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+  Future<int> insertCommande(Commande commande) async {
+    var r;
+    Database db = await this.db;
+    try {
+      r = await db.insert(tblCommande, commande.toMap());
+    } catch (e) {
+      debugPrint("insertCommande: " + e.toString());
+    }
+    return r;
+  }
+
+  Future<List> getCommandes() async {
+    Database db = await this.db;
+    var r =
+        await db.rawQuery("SELECT * FROM $tblCommande ORDER BY $nomClient ASC");
+    return r;
+  }
+
+  Future<List> getCommande(int id) async {
+    Database db = await this.db;
+    var r = await db.rawQuery(
+        "SELECT * FROM $tblCommande WHERE $idCommande = " + id.toString() + "");
+    return r;
+  }
+
+  Future<int> getCommandesCount() async {
+    Database db = await this.db;
+    var r = Sqflite.firstIntValue(
+        await db.rawQuery("SELECT COUNT(*) FROM $tblCommande"));
+    return r;
+  }
+
+  Future<int> getMaxIdCommande() async {
+    Database db = await this.db;
+    var r = Sqflite.firstIntValue(
+        await db.rawQuery("SELECT MAX(idCommande) FROM $tblCommande"));
+    return r;
+  }
+
+  Future<int> updateCommande(Commande commande) async {
+    var db = await this.db;
+    var r = await db.update(tblCommande, commande.toMap(),
+        where: "$idCommande = ?", whereArgs: [commande.idCommande]);
+    return r;
+  }
+
+  Future<int> deleteCommande(int id) async {
+    var db = await this.db;
+    int r =
+        await db.rawDelete("DELETE FROM $tblCommande WHERE $idCommande = $id");
+    return r;
+  }
+  /////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+////////////// Region ProduitCommande //////////////////
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+  Future<int> insertProduitCommande(ProduitCommande produitCommande) async {
+    var r;
+    Database db = await this.db;
+    try {
+      r = await db.insert(tblProduitCommande, produitCommande.toMap());
+    } catch (e) {
+      debugPrint("insertProduitCommande: " + e.toString());
+    }
+    return r;
+  }
+
+  Future<List> getProduitCommandes() async {
+    Database db = await this.db;
+    var r = await db.rawQuery(
+        "SELECT * FROM $tblProduitCommande ORDER BY $titreProduitCommande ASC");
+    return r;
+  }
+
+  Future<List> getProduitCommande(int id) async {
+    Database db = await this.db;
+    var r = await db.rawQuery(
+        "SELECT * FROM $tblProduitCommande WHERE $idProduitCommande = " +
+            id.toString() +
+            "");
+    return r;
+  }
+
+  Future<int> getProduitCommandesCount() async {
+    Database db = await this.db;
+    var r = Sqflite.firstIntValue(
+        await db.rawQuery("SELECT COUNT(*) FROM $tblProduitCommande"));
+    return r;
+  }
+
+  Future<int> getMaxIdProduitCommande() async {
+    Database db = await this.db;
+    var r = Sqflite.firstIntValue(await db
+        .rawQuery("SELECT MAX(idProduitCommande) FROM $tblProduitCommande"));
+    return r;
+  }
+
+  Future<int> updateProduitCommande(ProduitCommande produitCommande) async {
+    var db = await this.db;
+    var r = await db.update(tblProduitCommande, produitCommande.toMap(),
+        where: "$idProduitCommande = ?",
+        whereArgs: [produitCommande.idProduitCommande]);
+    return r;
+  }
+
+  Future<int> deleteProduitCommande(int id) async {
+    var db = await this.db;
+    int r = await db.rawDelete(
+        "DELETE FROM $tblProduitCommande WHERE $idProduitCommande = $id");
+    return r;
+  }
+
+  /////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+///////////////// Delete table //////////////////////
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+  Future<int> deleteRows(String tbl) async {
+    var db = await this.db;
+    int r = await db.rawDelete("DELETE FROM $tbl");
+    return r;
+  }
 }
--- lib/model/model.dart
+++ lib/model/model.dart
@@ -36,6 +36,35 @@ class Commande {
   String dateTime;
   int livraison;
   String descriptionCommande;
+
+  Commande(this.numTel, this.nomClient, this.dateTime, this.livraison,
+      this.descriptionCommande);
+
+  Commande.withId(this.idCommande, this.numTel, this.nomClient, this.dateTime,
+      this.livraison, this.descriptionCommande);
+
+  Commande.fromObjet(dynamic o) {
+    this.idCommande = o["idCommande"];
+    this.numTel = o["numTel"];
+    this.nomClient = o["nomClient"];
+    this.dateTime = o["dateTime"];
+    this.livraison = o["livraison"];
+    this.descriptionCommande = o["descriptionCommande"];
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    map["numTel"] = this.numTel;
+    map["nomClient"] = this.nomClient;
+    map["dateTime"] = this.dateTime;
+    map["livraison"] = this.livraison;
+    map["descriptionCommande"] = this.descriptionCommande;
+
+    if (this.idCommande != null) {
+      map["idCommande"] = this.idCommande;
+    }
+    return map;
+  }
 }
 
 class ProduitCommande {
@@ -44,4 +73,31 @@ class ProduitCommande {
   int quantite;
   int idCommande;
   int idProduit;
+
+  ProduitCommande(
+      this.titreProduit, this.quantite, this.idCommande, this.idProduit);
+
+  ProduitCommande.withId(this.idProduitCommande, this.titreProduit,
+      this.quantite, this.idCommande, this.idProduit);
+
+  ProduitCommande.fromObjet(dynamic o) {
+    this.idProduitCommande = o["idProduitCommande"];
+    this.titreProduit = o["titreProduit"];
+    this.quantite = o["quantite"];
+    this.idCommande = o["idCommande"];
+    this.idProduit = o["idProduit"];
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    map["titreProduit"] = this.titreProduit;
+    map["quantite"] = this.quantite;
+    map["idCommande"] = this.idCommande;
+    map["idProduit"] = this.idProduit;
+
+    if (this.idProduitCommande != null) {
+      map["idProduitCommande"] = this.idProduitCommande;
+    }
+    return map;
+  }
 }
